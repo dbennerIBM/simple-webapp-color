4de3e4d5cca9fb160dce17d5316afb8fcaaf8b9a
dbennerIBM <dbenner@us.ibm.com>
2019-04-22 14:43:46 -0500

    The Python app
    This app is forked from mmumshad from a repo that he no longer maintains.

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask
+from flask import render_template
+import socket
+import random
+
+app = Flask(__name__)
+
+color_codes = {
+    "red": "#e74c3c",
+    "green": "#16a085",
+    "blue": "#2980b9",
+    "orange": "#ff8000",
+    "pink": "#be2edd",
+    "yellow": "#ffff00"
+}
+    
+color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue2","darkblue","pink"])
+    
+@app.route("/")
+def main():
+    #return "Hello"
+    print(color)
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
+        
+@app.route('/color/<new_color>')
+def new_color(new_color):        
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080")
